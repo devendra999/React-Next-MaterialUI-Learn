cf731fb3115084d28c87c57051edc4fea4e21248
devendra999 <dev.kumar89.dp@gmail.com>
2022-03-24 13:09:50 +0530

    Material ui theme customisation with color like primary secondary color and how to use with other component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#000000',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#ffee51',
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log(theme);
+  return <>
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+    
+    </ThemeProvider>
+  </>
 }
 
 export default MyApp
