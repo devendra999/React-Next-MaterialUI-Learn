1d98ffaf04de3adfe639451b3873a806c29f7f3d
devendra999 <dev.kumar89.dp@gmail.com>
2022-04-01 15:23:03 +0530

    template literals

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,12 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 
   const fname = 'Devendra';
+  const lname = 'Prajapati';
 
   return (
     <>
-      <h1>My name is {fname}</h1>
-      <p>If you want to wright js in jsx you can use <b>{}</b></p>
+      <h1>{`My name is ${fname} ${lname}`}</h1>
+      <p>If you want to wright js and jsx both you can you backstick {` this is string ${fname} `} </p>
     </>
   )
 }
